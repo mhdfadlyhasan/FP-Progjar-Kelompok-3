d2ac3197aa080243ded10a285e273d120798c4a8
cluster777 <42055002+cluster777@users.noreply.github.com>
2020-05-11 13:56:31 +0700

    +relationship

--- dbmodel.py
+++ dbmodel.py
@@ -2,12 +2,13 @@ from django.db import models
 
 class Account:
     Acc_name = models.CharField(max_length=50)
-	Acc_email = models.CharField(max_length=50)
+    Acc_email = models.CharField(max_length=50)
 
 class Chat:
     Chat_name = models.CharField(max_length=50)
+    accounts = models.ManyToManyField(Account)
 
 class Message:
-    chat_id int,
-	msg = models.CharField(max_length=50)
-	get_time = models.DateTimeField()
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
+    msg = models.CharField(max_length=50)
+    get_time = models.DateTimeField()
