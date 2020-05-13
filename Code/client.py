a77b34e30513cb84196d1467775ee98f8b5fc40a
Mhd.Fadly Hasan <37266671+mhdfadlyhasan@users.noreply.github.com>
2020-05-13 12:23:42 +0700

    Send and recieve chat based on targeted username

--- Code/client.py
+++ Code/client.py
@@ -9,11 +9,13 @@ port = 8081
 username = input("Please enter your username: ")
 server.connect((ip_address, port))
 server.send(username.encode())
+username_target = input("Please enter targeted username: ")
+server.send(username_target.encode())
 
 while True:
     sockets_list = [sys.stdin, server]
 
-    ready_to_read = select.select([server], [], [], 1)[0]
+    ready_to_read = select.select([server], [], [], 0.01)[0]
     assert all(server.fileno() != -1 for server in ready_to_read)
     if msvcrt.kbhit(): ready_to_read.append(sys.stdin)
     # read_socket, write_socket, error_socket = select.select(sockets_list, [], [])
--- Code/server.py
+++ Code/server.py
@@ -10,40 +10,51 @@ port = 8081
 server.bind((ip_address, port))
 server.listen(100)
 list_of_clients = []
+tuple_of_clients = []
 
 def clientthread(conn, addr):
+    username_target = conn.recv(2048).decode()
+    print("Targeted Username " + username_target)
     while True:
         try:
             message = conn.recv(2048).decode()
             if message:
-                print (addr[2] + ':' + message[:-1])
-                message_to_send = addr[2] + ':' + message
-                broadcast(message_to_send, conn)
+                print (addr[2] + ': ' + message[:-1])
+                message_to_send = addr[2] + ': ' + message
+                broadcast(message_to_send, conn,addr[2],username_target)
             else:
                 remove(conn)
         except:
             continue
 
-def broadcast(message, connection):
-    for clients in list_of_clients:
-        if clients != connection:
+def broadcast(message, connection,username_sender,username_target):
+    for clients,data in list_of_clients:
+        if(data == username_target):
             try:
                 clients.send(message.encode())
             except:
                 clients.close()
                 remove(clients)
+    # for clients in list_of_clients:
+    #     if clients != connection:
+    #         try:
+    #             clients.send(message.encode())
+    #         except:
+    #             clients.close()
+    #             remove(clients)
 
 def remove(connection):
     if connection in list_of_clients:
         list_of_clients.remove(connection)
+        print("removed")
 
 while True:
     conn, addr = server.accept()
-    list_of_clients.append(conn)
     username = conn.recv(2048).decode()
     temp = list(addr)
     temp.append(username)
     addr = tuple(temp)
+    list_of_clients.append((conn,addr[2]))
     print (addr[2] + ' has joined the chat!')
     threading.Thread(target=clientthread, args=(conn, addr)).start()
     
