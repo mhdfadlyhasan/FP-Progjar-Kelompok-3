a3aa15cb13da643a0733fd3e7e7899b6120da986
cluster777 <42055002+cluster777@users.noreply.github.com>
2020-05-13 11:20:40 +0700

    ada sedikit perubahan

--- dbmodel.py
+++ dbmodel.py
@@ -6,9 +6,13 @@ class Account:
 
 class Chat:
     Chat_name = models.CharField(max_length=50)
-    accounts = models.ManyToManyField(Account)
+
+class Chat_Acc:
+    Acc_id = models.ForeignKey(Account, on_delete=models.CASCADE)
+    Chat_id = models.ForeignKey(Chat, on_delete=models.CASCADE)
 
 class Message:
     chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
     msg = models.CharField(max_length=50)
+    Acc_sent = models.ForeignKey(Account, on_delete=models.CASCADE)
     get_time = models.DateTimeField()
