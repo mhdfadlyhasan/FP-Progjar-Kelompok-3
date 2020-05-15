cb1c6004d72ebef38bdca1dce3ecf1810af6ab87
Mhd.Fadly Hasan <37266671+mhdfadlyhasan@users.noreply.github.com>
2020-05-15 07:48:10 +0700

    Update db.sql

--- db.sql
+++ db.sql
@@ -2,19 +2,19 @@ create table account(
 	id int auto_increment primary key,
 	name varchar(50),
 	email varchar(50)
-)
+);
 create table chatxaccount(
 	account_id int,
 	chat_id int
-)
+);
 create table chat(
 	id int auto_increment primary key,
 	chat_name varchar(50)
-)
+);
 create table message(
 	chat_id int,
 	msg varchar (500),
 	get_time datetime 
-)
+);
 
-insert into account(name,email)values('dummy','dummy@email.com')
+insert into account(name,email)values('dummy','dummy@email.com');
