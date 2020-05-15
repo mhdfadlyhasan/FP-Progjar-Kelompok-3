572fd3fc69ee7acb3468b8e1f873360ea42ba691
cluster777 <davidlaksmanasan@gmail.com>
2020-05-15 12:48:14 +0700

    login,chat udh || kurang form

--- chat/urls.py
+++ chat/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 
 from . import views
 
-app_name = 'ftp'
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    # ex: /polls/
+    path('', views.login, name='login'),
+    # ex: /polls/5/
+    path('<int:Acc_id>/', views.room_list, name='room_list'),
+    # ex: /polls/5/results/
+    path('<int:Acc_id>/<int:Chat_id>/', views.message_list, name='message_list'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -2,7 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Message
+from .models import Chat
+from django.template import loader
 
+def login(request):
+    return HttpResponse("ini menu login heheh")
 
-def index(request):
-    return HttpResponse("ini menu chat")
+def message_list(request, Chat_id,Acc_id):
+    msg_list = Message.objects.order_by('get_time')
+    template = loader.get_template('chat/message_list.html')
+    context = {
+        'message_list': msg_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def room_list(request,Acc_id):
+    chat_room_list = Chat.objects.order_by('Chat_name')
+    template = loader.get_template('chat/room_list.html')
+    context = {
+        'chat_room_list': chat_room_list,
+    }
+    return HttpResponse(template.render(context, request))
