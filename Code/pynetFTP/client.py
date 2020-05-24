5380ce610bfd63f69a261320b23265187d42dc27
Lix <prdptlx@gmail.com>
2020-05-24 12:38:59 +0700

    Client ftp

--- Code/pynetFTP/client.py
+++ Code/pynetFTP/client.py
@@ -25,19 +25,40 @@ class ClientFtp:
             list_res.append(file_path)
         return list_res
 
-    # def download(self, *args):
-    #     res = []
-    #
-    #     for arg in args:
-    #         fname = f"{arg}"
-    #         if Path(arg).is_dir():
-    #             with ZipFile(arg) as z:
-    #                 fname = f"{arg}.zip"
-    #                 z.write(filename=fname)
-    #
-    #         p = PurePath(fname).as_uri()
-    #         res.append(p)
-    #     return res
+    def download(self, *args):
+        res = []
+
+        for arg in args:
+            fname = f"{arg}"
+            if Path(arg).is_dir():
+                with ZipFile(arg) as z:
+                    fname = f"{arg}.zip"
+                    z.write(filename=fname)
+
+            with open(fname, 'wb') as fd:
+                self.server.retrbinary('RETR ' + fname, fd.write, 1024)
+                res.append(fd)
+                print(fd)
+
+        return res
+
+    def upload(self, *args):
+        res = []
+
+        for arg in args:
+            with open(arg, 'rb') as fd:
+                self.server.storbinary('STOR ', arg, fd, 1024)
+
+    def changedir(self, **kwargs):
+        global to_path
+        if kwargs is not None:
+            for key, val in kwargs.items():
+                if key == 'from':
+                    from_path = val
+                elif key == 'to':
+                    to_path = val
+
+            self.server.cwd(to_path)
 
 
 if __name__ == '__main__':
