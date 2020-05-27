ac580c41661b953aa034d8e11f39ba2646754862
Octavianus Giovanni Y <octavianusgiovanni@gmail.com>
2020-05-27 14:23:16 +0700

    Rename models
    *ganti juga yg pake model itu

--- chat/models.py
+++ chat/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+# Rename nama Room dan Room_Acc
 class Room(models.Model):
     Room_name = models.CharField(max_length=50)
 
