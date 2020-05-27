137b607ebac4604cf67cb08d75b1a2930c03fcb1
ArisatoX <patricksungkharisma27@gmail.com>
2020-05-27 18:21:47 +0700

    change group chat window GUI

--- GUI/GroupChat/view/Ui_group_chat_window.py
+++ GUI/GroupChat/view/Ui_group_chat_window.py
@@ -19,56 +19,39 @@ class Ui_group_chat_window(object):
         self.show_member.setGeometry(QtCore.QRect(690, 10, 75, 23))
         self.show_member.setObjectName("show_member")
         self.group_name = QtWidgets.QLabel(self.centralwidget)
-        self.group_name.setGeometry(QtCore.QRect(80, 0, 91, 51))
+        self.group_name.setGeometry(QtCore.QRect(30, 0, 91, 51))
         self.group_name.setObjectName("group_name")
         self.upper_line = QtWidgets.QFrame(self.centralwidget)
         self.upper_line.setGeometry(QtCore.QRect(0, 50, 801, 16))
         self.upper_line.setFrameShape(QtWidgets.QFrame.HLine)
         self.upper_line.setFrameShadow(QtWidgets.QFrame.Sunken)
         self.upper_line.setObjectName("upper_line")
-        self.scrollbar = QtWidgets.QScrollBar(self.centralwidget)
-        self.scrollbar.setGeometry(QtCore.QRect(780, 60, 16, 391))
-        self.scrollbar.setOrientation(QtCore.Qt.Vertical)
-        self.scrollbar.setObjectName("scrollbar")
         self.bottomline = QtWidgets.QFrame(self.centralwidget)
-        self.bottomline.setGeometry(QtCore.QRect(-3, 440, 801, 20))
+        self.bottomline.setGeometry(QtCore.QRect(-3, 490, 801, 20))
         self.bottomline.setFrameShape(QtWidgets.QFrame.HLine)
         self.bottomline.setFrameShadow(QtWidgets.QFrame.Sunken)
         self.bottomline.setObjectName("bottomline")
-        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
-        self.textEdit.setGeometry(QtCore.QRect(10, 490, 781, 81))
-        self.textEdit.setObjectName("textEdit")
         self.file = QtWidgets.QPushButton(self.centralwidget)
-        self.file.setGeometry(QtCore.QRect(10, 460, 75, 23))
+        self.file.setGeometry(QtCore.QRect(10, 510, 75, 23))
         self.file.setObjectName("file")
-        self.member_name_container = QtWidgets.QWidget(self.centralwidget)
-        self.member_name_container.setGeometry(QtCore.QRect(10, 60, 761, 81))
-        self.member_name_container.setObjectName("member_name_container")
-        self.verticalLayoutWidget = QtWidgets.QWidget(self.member_name_container)
-        self.verticalLayoutWidget.setGeometry(QtCore.QRect(0, 0, 761, 80))
-        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
-        self.member_name_layout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
-        self.member_name_layout.setContentsMargins(0, 0, 0, 0)
-        self.member_name_layout.setObjectName("member_name_layout")
-        self.member_name = QtWidgets.QLabel(self.verticalLayoutWidget)
-        self.member_name.setFrameShape(QtWidgets.QFrame.Box)
-        self.member_name.setObjectName("member_name")
-        self.member_name_layout.addWidget(self.member_name)
         self.invite = QtWidgets.QPushButton(self.centralwidget)
         self.invite.setGeometry(QtCore.QRect(610, 10, 75, 23))
         self.invite.setObjectName("invite")
+        self.message_list = QtWidgets.QTextEdit(self.centralwidget)
+        self.message_list.setGeometry(QtCore.QRect(10, 70, 781, 421))
+        self.message_list.setObjectName("message_list")
+        self.input = QtWidgets.QLineEdit(self.centralwidget)
+        self.input.setGeometry(QtCore.QRect(10, 540, 781, 31))
+        self.input.setObjectName("input")
         group_chat_window.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(group_chat_window)
         self.statusbar.setObjectName("statusbar")
         group_chat_window.setStatusBar(self.statusbar)
+
         self.retranslateUi(group_chat_window)
         QtCore.QMetaObject.connectSlotsByName(group_chat_window)
 
         # Custom
-        self.member_name_container.hide()
-
-        # Connecting to functions
-        self.show_member.clicked.connect(self.member_list)
 
     def retranslateUi(self, group_chat_window):
         _translate = QtCore.QCoreApplication.translate
@@ -76,15 +59,7 @@ class Ui_group_chat_window(object):
         self.show_member.setText(_translate("group_chat_window", "Members"))
         self.group_name.setText(_translate("group_chat_window", "Group Name"))
         self.file.setText(_translate("group_chat_window", "File"))
-        self.member_name.setText(_translate("group_chat_window", " Members Name"))
         self.invite.setText(_translate("group_chat_window", "Invite"))
-
-    # Button click testing
-    def member_list(self):
-        if self.member_name_container.isVisible():
-            self.member_name_container.hide()
-        else:
-            self.member_name_container.show()
 
 if __name__ == "__main__":
     import sys
