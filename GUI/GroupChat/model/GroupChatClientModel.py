9ba99717bfc01c0d85143a164b793c19c2553314
ArisatoX <patricksungkharisma27@gmail.com>
2020-05-27 19:21:27 +0700

    changed groupchat client and server code to OOP

--- GUI/GroupChat/model/GroupChatClientModel.py
+++ GUI/GroupChat/model/GroupChatClientModel.py
@@ -3,111 +3,79 @@ import select
 import sys
 import msvcrt
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ip_address = '127.0.0.1'
-port = 8081
+class GroupChatClientModel:
+    isConnected = False
+    server = None
 
-# Input username & ID sendiri
-username = input("Please enter your username: ")
-your_id = input("Please enter your ID: ")
-server.connect((ip_address, port))
+    def __init__(self):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Send username + id
-packet = username + ',' + your_id
-server.send(packet.encode())
+    def connect(self):
+        ip_address = '127.0.0.1'
+        port = 8081
 
-while True:
+        # Input username & ID sendiri
+        username = input("Please enter your username: ")
+        your_id = input("Please enter your ID: ")
+        self.server.connect((ip_address, port))
+        self.isConnected = True
 
-    # Pilih mode
-    print("Type the mode's number that you want to use\n1. Personal Chat\n2. Group Chat\n3. Quit\n")
-    mode = input('Mode: ')
+        # Send username + id
+        packet = username + ',' + your_id
+        self.server.send(packet.encode())
 
-    # Personal Chat
-    if (mode == '1'):
+        return self
 
-        # Input id orang yang ingin di chat
-        personal_id = input("Please enter the person's ID: ")
-        personal = '<id>' + personal_id
-        # print(personal)
-        server.send(personal.encode())
+    def group_chat(self):
 
-        while True:
-            sockets_list = [sys.stdin, server]
+        sockets_list = [sys.stdin, self.server]
 
-            # Tunggu input
-            ready_to_read = select.select([server], [], [], 0.01)[0] #delay diperkecil biar lebih responsive
-            assert all(server.fileno() != -1 for server in ready_to_read)
-            if msvcrt.kbhit(): ready_to_read.append(sys.stdin)
+        # Tunggu input
+        ready_to_read = select.select([self.server], [], [], 0.01)[0] #delay diperkecil biar lebih responsive
+        assert all(self.server.fileno() != -1 for self.server in ready_to_read)
+        if msvcrt.kbhit(): ready_to_read.append(sys.stdin)
 
-            for socks in ready_to_read:
-                # Terima message
-                if socks == server:
-                    message = socks.recv(2048).decode()
-                    print(message)
+        for socks in ready_to_read:
+            # Terima message
+            if socks == self.server:
+                message = socks.recv(2048).decode()
+                print(message)
 
-                # Send message
+            # Send message
+            else:
+                message = sys.stdin.readline()
+                # Format <create> ID_room
+                if (message[:8] == '<create>'):
+                    split = message.split(' ')
+                    print ('You created a room with ID ' + split[1])
+                    self.server.send(message.encode())
+
+                # Format <invite> ID_user
+                elif (message[:8] == '<invite>'):
+                    split = message.split(' ')
+                    print ('You invited user with ID ' + split[1])
+                    self.server.send(message.encode())   
+
+                # Format <join> ID_room
+                elif (message[:6] == '<join>'):
+                    split = message.split(' ')
+                    print ('You joined room with ID ' + split[1])
+                    self.server.send(message.encode())
+                    
                 else:
-                    message = sys.stdin.readline()
-                    message = '<personal>' + message
-                    server.send(message.encode())
+                    message = '<group>' + message
+                    # print(message)
+                    self.server.send(message.encode())
+                    sys.stdout.flush()
                 
                 sys.stdout.flush()
 
-    # Group Chat
-    elif (mode == '2'):
+if __name__ == '__main__':
+    model = GroupChatClientModel()
+    model.connect()
 
-        while True:
-            sockets_list = [sys.stdin, server]
-
-            # Tunggu input
-            ready_to_read = select.select([server], [], [], 0.01)[0] #delay diperkecil biar lebih responsive
-            assert all(server.fileno() != -1 for server in ready_to_read)
-            if msvcrt.kbhit(): ready_to_read.append(sys.stdin)
-
-            for socks in ready_to_read:
-                # Terima message
-                if socks == server:
-                    message = socks.recv(2048).decode()
-                    print(message)
-
-                # Send message
-                else:
-                    message = sys.stdin.readline()
-                    # Format <create> ID_room
-                    if (message[:8] == '<create>'):
-                        split = message.split(' ')
-                        print ('You created a room with ID ' + split[1])
-                        server.send(message.encode())
-
-                    # Format <invite> ID_user
-                    elif (message[:8] == '<invite>'):
-                        split = message.split(' ')
-                        print ('You invited user with ID ' + split[1])
-                        server.send(message.encode())   
-
-                    # Format <join> ID_room
-                    elif (message[:6] == '<join>'):
-                        split = message.split(' ')
-                        print ('You joined room with ID ' + split[1])
-                        server.send(message.encode())
-                        
-                    else:
-                        message = '<group>' + message
-                        # print(message)
-                        server.send(message.encode())
-                        sys.stdout.flush()
-                    
-                    sys.stdout.flush()
-
-    # Quit
-    elif (mode == '3'):
-        message = '<quit>'
-        server.send(message.encode())
-        server.close()
-        sys.exit()
-
-    # Mode Error Handler
-    else:
-        print('Wrong mode please try again')
+    while True:
+        model.group_chat()
+        
 
 
--- GUI/GroupChat/model/GroupChatServerModel.py
+++ GUI/GroupChat/model/GroupChatServerModel.py
@@ -3,131 +3,121 @@ import select
 import sys
 import threading
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-ip_address = '127.0.0.1'
-port = 8081
-server.bind((ip_address, port))
-server.listen(100)
-list_of_clients = []
-room_example = []
-unique_id = ""
+class GroupChatServerModel:
 
-def clientthread(conn, addr):
+    list_of_clients = []
+    room_example = []
+    unique_id = ""
+    server = None
     
+    def __init__(self):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        ip_address = '127.0.0.1'
+        port = 8081
+        self.server.bind((ip_address, port))
+        self.server.listen(100)
+
+    def clientthread(self, conn, addr):
+        
+        while True:
+            try:
+                message = conn.recv(2048).decode()
+
+                # Client berhenti
+                if (message[:6] == '<quit>'):
+                    print('Client with ID ' + str(addr[3]) + ' has left the application')
+
+                elif (message[:8] == '<create>'):
+
+                    split = message.split(' ')
+
+                    # code create room di DB disini
+
+                    # Room dummy untuk testing awal
+                    self.room_example.append((conn, str(addr[3])))
+                    print('Room Created with ID ' + split[1]) 
+
+                elif (message[:8] == '<invite>'):
+                    split = message.split(' ')
+                    invite_id = split[1]
+
+                    for client in self.list_of_clients:
+                        if (client[1] == invite_id[:-1]):
+                            print("Receiver ID: " + client[1])
+                            client_conn = client[0]
+                            # print(client_conn)
+
+                    self.room_example.append((client_conn, invite_id[:-1])) 
+                    # print (room_example)
+
+                elif (message[:6] == '<join>'):
+                    print('join')
+
+                # Terima id orang yang akan di personal chat
+                elif (message[:4] == '<id>'):
+                    unique_id = message[4:len(message)]
+                    print(unique_id)
+                    #tunjukkan list pesan ke pengguna! list didapat dari dataase
+                    message = "selamat datang, riwayat pesan anda "
+                    conn.send(message.encode())
+
+                # Send message group chat
+                elif (message[:7] == '<group>'):
+                    message_to_send = addr[2] + ':' + message[7:-1]
+                    sender_id = str(addr[3])
+                    print (addr[2] + ':' + message[7:-1])
+
+                    # Code broadcast dengan room id disini
+
+                    # broadcast dengan room dummy
+                    self.broadcast(message_to_send, conn, sender_id)
+
+                    #put to db
+
+                else:
+                    self.remove(conn)
+            except:
+                continue
+
+    def broadcast(self, message, connection, sender_id):
+        for clients, unique_id in self.room_example:
+            if unique_id != sender_id:
+                try:
+                    clients.send(message.encode())
+                except:
+                    clients.close()
+                    self.remove_group(clients)
+
+    def remove(self, connection):
+        if connection in self.list_of_clients:
+            self.list_of_clients.remove(connection)
+
+    def remove_group(self, connection):
+        if connection in self.room_example:
+            self.room_example.remove(connection)
+
+    def server_start(self):
+        conn, addr = self.server.accept()
+        packet = conn.recv(2048).decode()
+
+        # Menambahkan username dan id ke addr
+        split = packet.split(',')
+        # print(split)
+        temp = list(addr)
+        temp.append(split[0])
+        temp.append(split[1])
+        addr = tuple(temp)
+
+        # Register id
+        self.list_of_clients.append((conn, str(addr[3]))) 
+        print (str(addr[2]) + ' has joined the chat with ID ' + str(addr[3]))
+        # print(conn)
+        threading.Thread(target=self.clientthread, args=(conn, addr)).start()
+
+if __name__ == '__main__':
+
+    model = GroupChatServerModel()
+
     while True:
-        try:
-            message = conn.recv(2048).decode()
-
-            # Client berhenti
-            if (message[:6] == '<quit>'):
-                print('Client with ID ' + str(addr[3]) + ' has left the application')
-
-            elif (message[:8] == '<create>'):
-
-                split = message.split(' ')
-
-                # code create room di DB disini
-
-                # Room dummy untuk testing awal
-                room_example.append((conn, str(addr[3])))
-                print('Room Created with ID ' + split[1]) 
-
-            elif (message[:8] == '<invite>'):
-                split = message.split(' ')
-                invite_id = split[1]
-
-                for client in list_of_clients:
-                    if (client[1] == invite_id[:-1]):
-                        print("Receiver ID: " + client[1])
-                        client_conn = client[0]
-                        # print(client_conn)
-
-                room_example.append((client_conn, invite_id[:-1])) 
-                print (room_example)
-
-            elif (message[:6] == '<join>'):
-                print('join')
-
-            # Terima id orang yang akan di personal chat
-            elif (message[:4] == '<id>'):
-                unique_id = message[4:len(message)]
-                print(unique_id)
-                #tunjukkan list pesan ke pengguna! list didapat dari dataase
-                message = "selamat datang, riwayat pesan anda "
-                conn.send(message.encode())
-
-            # Send message personal chat
-            elif (message[:10] == '<personal>'):
-                print (addr[2] + ':' + message[10:-1])
-                message_to_send = addr[2] + ':' + message[10:-1]
-                personal_chat(message_to_send, conn, addr[3], unique_id)
-                #put to db
-
-            # Send message personal chat
-            elif (message[:7] == '<group>'):
-                message_to_send = addr[2] + ':' + message[7:-1]
-                sender_id = str(addr[3])
-                print (addr[2] + ':' + message[7:-1])
-
-                # Code broadcast dengan room id disini
-
-                # broadcast dengan room dummy
-                broadcast(message_to_send, conn, sender_id)
-
-                #put to db
-
-            else:
-                remove(conn)
-        except:
-            continue
-
-def personal_chat(message, connection, sender_id, receiver_id):
-    # Mencari id orang yang akan dikirimi pesan
-    for clients, unique_id in list_of_clients:
-        if unique_id == receiver_id:
-            try:
-                clients.send(message.encode())
-            except:
-                message = 'User with ID ' + str(receiver_id) + ' has left\n'
-                print(message)
-                clients.close()
-                remove(clients)
-                personal_chat(message, connection, sender_id, sender_id)
-
-def broadcast(message, connection, sender_id):
-    for clients, unique_id in room_example:
-        if unique_id != sender_id:
-            try:
-                clients.send(message.encode())
-            except:
-                clients.close()
-                remove_group(clients)
-
-def remove(connection):
-    if connection in list_of_clients:
-        list_of_clients.remove(connection)
-
-def remove_group(connection):
-    if connection in room_example:
-        room_example.remove(connection)
-
-while True:
-    conn, addr = server.accept()
-    packet = conn.recv(2048).decode()
-
-    # Menambahkan username dan id ke addr
-    split = packet.split(',')
-    # print(split)
-    temp = list(addr)
-    temp.append(split[0])
-    temp.append(split[1])
-    addr = tuple(temp)
-
-    # Register id
-    list_of_clients.append((conn, str(addr[3]))) 
-    print (str(addr[2]) + ' has joined the chat with ID ' + str(addr[3]))
-    # print(conn)
-    threading.Thread(target=clientthread, args=(conn, addr)).start()
-    
-conn.close()
+        model.server_start()
