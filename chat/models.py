408321864591d664bd22b375b734ee0c4b6bb93d
Octavianus Giovanni Y <octavianusgiovanni@gmail.com>
2020-05-30 22:15:58 +0700

    Update models (pakai Foreign Key)

--- chat/models.py
+++ chat/models.py
@@ -8,15 +8,15 @@ class Room(models.Model):
     RoomName = models.CharField(max_length=50)
 
 class Room_Acc(models.Model):
-    # AccID = models.ForeignKey(User, on_delete=models.CASCADE)
-    AccID = models.IntegerField() #dummy ID save
+    AccID = models.ForeignKey(User, on_delete=models.CASCADE)
+    # AccID = models.IntegerField() #dummy ID save
     RoomID = models.ForeignKey(Room, on_delete=models.CASCADE)
 
 
 class Message(models.Model):
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     msg = models.CharField(max_length=50)
-    # AccSent = models.ForeignKey(User, on_delete=models.CASCADE)
-    AccSent = models.IntegerField() #dummy ID sender
+    AccSent = models.ForeignKey(User, on_delete=models.CASCADE)
+    # AccSent = models.IntegerField() #dummy ID sender
     getTime = models.DateTimeField()
 
