f5733864e20493f0b198c7c2e7400c9f5a649da7
Mhd.Fadly Hasan <37266671+mhdfadlyhasan@users.noreply.github.com>
2020-05-31 16:13:28 +0700

    Update join method, now user can enter the chat whitout sending <join>

--- GUI/GroupChat/app.py
+++ GUI/GroupChat/app.py
@@ -21,7 +21,10 @@ class ChatList(QMainWindow, Ui_ChatList):
         self.setupUi(self)
         # Panggil function untuk start thread
         self.client_run(connection)
-        
+        connection.server.send(("<roomlist> " + connection.your_id).encode())
+        print("sending all room details!!!")
+        message = connection.server.recv(2048).decode()
+        print(message)
         # Temp groupchat window
         self.chat_list.itemActivated.connect(self.chat_click)
         #list semua room disini?
